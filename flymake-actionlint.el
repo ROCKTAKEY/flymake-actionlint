e16cc72f6143a60b5283ca282b31f1e1a23d2683
ROCKTAKEY <rocktakey@gmail.com>
2023-07-31 16:48:01 +0900

    docs: Add README and commentary

--- flymake-actionlint.el
+++ flymake-actionlint.el
@@ -24,7 +24,64 @@
 
 ;;; Commentary:
 
-;; A Flymake handler for actionlint
+;; Table of Contents
+;; _________________
+
+;; 1. flymake-actionlint: A Flymake handler for actionlint
+;; 2. How to Use?
+;; 3. License
+
+
+;; [https://img.shields.io/github/tag/ROCKTAKEY/flymake-actionlint.svg?style=flat-square]
+;; [https://img.shields.io/github/license/ROCKTAKEY/flymake-actionlint.svg?style=flat-square]
+;; [https://img.shields.io/codecov/c/github/ROCKTAKEY/flymake-actionlint.svg?style=flat-square]
+;; [https://img.shields.io/github/actions/workflow/status/ROCKTAKEY/flymake-actionlint/test.yml.svg?branch=master&style=flat-square]
+
+
+;; [https://img.shields.io/github/tag/ROCKTAKEY/flymake-actionlint.svg?style=flat-square]
+;; <https://github.com/ROCKTAKEY/flymake-actionlint>
+
+;; [https://img.shields.io/github/license/ROCKTAKEY/flymake-actionlint.svg?style=flat-square]
+;; <file:LICENSE>
+
+;; [https://img.shields.io/codecov/c/github/ROCKTAKEY/flymake-actionlint.svg?style=flat-square]
+;; <https://codecov.io/gh/ROCKTAKEY/flymake-actionlint?branch=master>
+
+;; [https://img.shields.io/github/actions/workflow/status/ROCKTAKEY/flymake-actionlint/test.yml.svg?branch=master&style=flat-square]
+;; <https://github.com/ROCKTAKEY/flymake-actionlint/actions>
+
+
+;; 1 flymake-actionlint: A Flymake handler for actionlint
+;; ======================================================
+
+;;   Provide `Flymake' handler for [actionlint].
+
+
+;; [actionlint] <https://github.com/rhysd/actionlint>
+
+
+;; 2 How to Use?
+;; =============
+
+;;   ,----
+;;   | (add-hook 'yaml-mode-hook #'flymake-actionlint-action-load-when-actions-file)
+;;   `----
+;;   `flymake-actionlint-action-load-when-actions-file' automatically turns
+;;   `flymake-actionlint' on only when the current file is a yaml file in
+;;   `.github/workflows/'.
+
+;;   Run `M-x flymake-actionlint-load' if you want to turn
+;;   `flymake-actionlint' on manually.
+
+
+;; 3 License
+;; =========
+
+;;   This package is licensed by The GNU General Public License verson 3 or
+;;   later. See [LICENSE].
+
+
+;; [LICENSE] <file:LICENSE>
 
 ;;; Code:
 
